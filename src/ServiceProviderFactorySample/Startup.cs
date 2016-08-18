b8dee5e2d4d0e84faff8a0555ca6e0834d096ea6
David Fowler <davidfowl@gmail.com>
2016-08-17 17:43:45 -0700

    Initial commit

---
+++ src/ServiceProviderFactorySample/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ServiceProviderFactorySample
+{
+    public class Startup
+    {
+        public void ConfigureContainer(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<Foo>().AsImplementedInterfaces().SingleInstance();
+        }
+
+        public void Configure(IApplicationBuilder app, Lazy<IFoo> foo)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello World! {foo.Value.GetHashCode()}");
+            });
+        }
+
+        public class Foo : IFoo
+        {
+
+        }
+
+        public interface IFoo
+        {
+
+        }
+    }
+}
